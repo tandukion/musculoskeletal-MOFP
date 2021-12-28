0ec583a0ad55902bd2af8c6753fa78f574ee6c9c
Dwindra Sulistyoutomo <tandukion@hotmail.com>
2021-12-28 19:05:54 +0900

    Change GUI to units and added uitable

--- mainMOFPGUI.m
+++ mainMOFPGUI.m
@@ -24,9 +24,10 @@ f = f_uniform;
 
 k = k_uniform;
 
-figure(1, "position",[400,400,1200,900]);
+figure(1, "position",[400,400,1400,900]);
 
-h.ax = axes ("position", [0.05 2.5/9 5/12 6/9]);
+ax_pos = [50 50 700 800];
+h.ax = axes ("units", "pixels", "position", ax_pos);
 
 % ============================ Functions ============================
 
@@ -110,6 +111,19 @@ function updatePlot (obj)
             theta(3) = str2double(theta_str)*pi/180;
             set (h.ankle_joint_slider, "value", theta(3));
             recalc = true;
+        
+        % G
+        case {h.MA_table}
+            G = get (h.MA_table, "Data");
+            recalc = true;
+        % f
+        case {h.f_table}
+            f = get (h.f_table, "Data");
+            recalc = true;
+        % k
+        case {h.k_table}
+            k = get (h.k_table, "Data");
+            % recalc = true;
     end
 
     if (recalc)
@@ -121,138 +135,181 @@ function updatePlot (obj)
 end
 
 % ============================ GUI settings ============================
+setting_posX = ax_pos(1)+ax_pos(3) + 20;
+setting_posY = ax_pos(2)+ax_pos(4) - 20;
 
 % Link length
-link_length_posX = 0.01;
-link_length_posY = 0.2;
-link_length_w = 0.05;
-link_length_h = 0.02;
+link1_pos = [setting_posX setting_posY-20 50 20];
+link2_pos = [setting_posX setting_posY-40 50 20];
+link3_pos = [setting_posX setting_posY-60 50 20];
 h.link1_label = uicontrol ("style", "text",
-                            "units", "normalized",
                             "string", "Link1: ",
                             "horizontalalignment", "left",
-                            "position", [link_length_posX link_length_posY link_length_w 0.02]);
+                            "position", link1_pos);
 h.link1_value = uicontrol ("style", "edit",
-                            "units", "normalized",
                             "string", sprintf ("%.3f", L(1)),
                             "callback", @updatePlot,
-                            "position", [link_length_posX+link_length_w link_length_posY link_length_w link_length_h]);
+                            "position", link1_pos + [50 0 25 0]);
 h.m1_label = uicontrol ("style", "text",
-                        "units", "normalized",
                         "string", " m",
                         "horizontalalignment", "left",
-                        "position", [link_length_posX+2*link_length_w link_length_posY 0.03 0.02]);
+                        "position", link1_pos + [125 0 -25 0]);
 
 h.link2_label = uicontrol ("style", "text",
-                            "units", "normalized",
                             "string", "Link2: ",
                             "horizontalalignment", "left",
-                            "position", [link_length_posX link_length_posY-link_length_h link_length_w 0.02]);
+                            "position", link2_pos);
 h.link2_value = uicontrol ("style", "edit",
-                            "units", "normalized",
                             "string", sprintf ("%.3f", L(2)),
                             "callback", @updatePlot,
-                            "position", [link_length_posX+link_length_w link_length_posY-link_length_h link_length_w link_length_h]);
+                            "position", link2_pos + [50 0 25 0]);
 h.m2_label = uicontrol ("style", "text",
-                        "units", "normalized",
                         "string", " m",
                         "horizontalalignment", "left",
-                        "position", [link_length_posX+2*link_length_w link_length_posY-link_length_h 0.03 0.02]);
+                        "position", link2_pos + [125 0 -25 0]);
 
 h.link3_label = uicontrol ("style", "text",
-                            "units", "normalized",
                             "string", "Link3: ",
                             "horizontalalignment", "left",
-                            "position", [link_length_posX link_length_posY-2*link_length_h link_length_w 0.02]);
+                            "position", link3_pos);
 h.link3_value = uicontrol ("style", "edit",
-                            "units", "normalized",
                             "string", sprintf ("%.3f", L(3)),
                             "callback", @updatePlot,
-                            "position", [link_length_posX+link_length_w link_length_posY-2*link_length_h link_length_w link_length_h]);
+                            "position", link3_pos + [50 0 25 0]);
 h.m3_label = uicontrol ("style", "text",
-                        "units", "normalized",
                         "string", " m",
                         "horizontalalignment", "left",
-                        "position", [link_length_posX+2*link_length_w link_length_posY-2*link_length_h 0.03 0.02]);
+                        "position", link3_pos + [125 0 -25 0]);
 
 % Joint Slider
-joint_slider_posX = 0.15;
-joint_slider_posY = 0.2;
-joint_slider_w = 0.25;
-joint_slider_h = 0.02;
+joint_setting_posX = setting_posX + 180;
+joint_setting_posY = setting_posY;
+joint1_pos = [joint_setting_posX joint_setting_posY 0 0];
+joint2_pos = [joint_setting_posX joint_setting_posY-40 0 0];
+joint3_pos = [joint_setting_posX joint_setting_posY-80 0 0];
+
 h.hip_joint_label = uicontrol ( "style", "text",
-                                "units", "normalized",
                                 "string", "Hip joint",
                                 "horizontalalignment", "left",
-                                "position", [joint_slider_posX joint_slider_posY joint_slider_w joint_slider_h]);
+                                "position", joint1_pos + [0 0 100 20]);
 h.hip_joint_slider = uicontrol ("style", "slider",
-                                "units", "normalized",
                                 "string", "slider",
                                 "callback", @updatePlot,
                                 "value", theta(1),
                                 "max", pi/2,
                                 "min", -pi/2,
-                                "position", [joint_slider_posX joint_slider_posY-joint_slider_h joint_slider_w joint_slider_h]);
+                                "position", joint1_pos + [0 -20 250 20]);
 h.hip_joint_value = uicontrol ( "style", "edit",
-                                "units", "normalized",
                                 "string", sprintf("%.1f", theta(1)*180/pi),
                                 "callback", @updatePlot,
-                                "position", [joint_slider_posX+joint_slider_w+0.005 joint_slider_posY-joint_slider_h 0.05 0.02]);
+                                "position", joint1_pos + [275 -20 75 20]);
 h.deg1_label = uicontrol (  "style", "text",
-                            "units", "normalized",
                             "string", " deg",
                             "horizontalalignment", "left",
-                            "position", [joint_slider_posX+joint_slider_w+0.055 joint_slider_posY-joint_slider_h 0.03 0.02]);
+                            "position", joint1_pos + [350 -20 50 20]);
 
 h.knee_joint_label = uicontrol ("style", "text",
-                                "units", "normalized",
                                 "string", "Knee joint",
                                 "horizontalalignment", "left",
-                                "position", [joint_slider_posX joint_slider_posY-3*joint_slider_h joint_slider_w joint_slider_h]);
+                                "position", joint2_pos + [0 0 100 20]);
 h.knee_joint_slider = uicontrol (   "style", "slider",
-                                    "units", "normalized",
                                     "string", "slider",
                                     "callback", @updatePlot,
                                     "value", theta(2),
                                     "max", pi/6,
                                     "min", -pi,
-                                    "position", [joint_slider_posX joint_slider_posY-4*joint_slider_h joint_slider_w joint_slider_h]);
+                                    "position", joint2_pos + [0 -20 250 20]);
 h.knee_joint_value = uicontrol ("style", "edit",
-                                "units", "normalized",
                                 "string", sprintf("%.1f", theta(2)*180/pi),
                                 "callback", @updatePlot,
-                                "position", [joint_slider_posX+joint_slider_w+0.005 joint_slider_posY-4*joint_slider_h 0.05 0.02]);
+                                "position", joint2_pos + [275 -20 75 20]);
 h.deg2_label = uicontrol (  "style", "text",
-                            "units", "normalized",
                             "string", " deg",
                             "horizontalalignment", "left",
-                            "position", [joint_slider_posX+joint_slider_w+0.055 joint_slider_posY-4*joint_slider_h 0.03 0.02]);
+                            "position", joint2_pos + [350 -20 50 20]);
 
 h.ankle_joint_label = uicontrol (   "style", "text",
-                                    "units", "normalized",
                                     "string", "Ankle joint",
                                     "horizontalalignment", "left",
-                                    "position", [joint_slider_posX joint_slider_posY-6*joint_slider_h joint_slider_w joint_slider_h]);
+                                    "position", joint3_pos + [0 0 100 20]);
 h.ankle_joint_slider = uicontrol (  "style", "slider",
-                                    "units", "normalized",
                                     "string", "slider",
                                     "callback", @updatePlot,
                                     "value", theta(3),
                                     "max", pi,
                                     "min", -pi/3,
-                                    "position", [joint_slider_posX joint_slider_posY-7*joint_slider_h joint_slider_w joint_slider_h]);
+                                    "position", joint3_pos + [0 -20 250 20]);
 h.ankle_joint_value = uicontrol (   "style", "edit",
-                                    "units", "normalized",
                                     "string", sprintf("%.1f", theta(3)*180/pi),
                                     "callback", @updatePlot,
-                                    "position", [joint_slider_posX+joint_slider_w+0.005 joint_slider_posY-7*joint_slider_h 0.05 0.02]);
+                                    "position", joint3_pos + [275 -20 75 20]);
 h.deg3_label = uicontrol (  "style", "text",
-                            "units", "normalized",
                             "string", " deg",
                             "horizontalalignment", "left",
-                            "position", [joint_slider_posX+joint_slider_w+0.055 joint_slider_posY-7*joint_slider_h 0.03 0.02]);
+                            "position", joint3_pos + [350 -20 50 20]);
 
- 
+% ------ Table based GUI ------
+table_col = 50;
+
+% G (Moment arm) table
+MA_table_posX = setting_posX;
+MA_table_posY = setting_posY-400;
+MA_table_h = 235;
+MA_table_pos = [MA_table_posX MA_table_posY 200 MA_table_h];
+h.MA_table_label = uicontrol ( "style", "text",
+                                "string", "Moment arms (m)",
+                                "horizontalalignment", "center",
+                                "position", [MA_table_pos(1)+50 MA_table_pos(2)+MA_table_pos(4) 150 20]
+);
+h.MA_table = uitable (  "Data", G,
+                        "ColumnEditable",true,
+                        "ColumnName", {"hip";"knee";"ankle"},
+                        "RowName", cellstr(mus_name),
+                        "CellEditCallback", @updatePlot,
+                        "ColumnWidth", repmat({table_col}, 1, size(G,2)),
+                        "Position", MA_table_pos
+);
+
+% Force table
+f_table_col = 60;
+f_table_posX = setting_posX + 210;
+f_table_posY = MA_table_posY;
+f_table_h = MA_table_h;
+f_table_pos = [f_table_posX f_table_posY f_table_col f_table_h];
+h.f_table_label = uicontrol (   "style", "text",
+                                "string", "Force (N)",
+                                "horizontalalignment", "center",
+                                "position", [f_table_pos(1) f_table_pos(2)+f_table_pos(4) f_table_pos(3) 20]
+);
+h.f_table = uitable (   "Data", f,
+                        "ColumnEditable",true,
+                        "ColumnName", {"f"},
+                        "RowName", "",
+                        "CellEditCallback", @updatePlot,
+                        "ColumnWidth", repmat({f_table_col}, 1, size(f,2)),
+                        "Position", f_table_pos
+);
+
+% Stifness table
+k_table_col = 60;
+k_table_posX = setting_posX + 275;
+k_table_posY = MA_table_posY;
+k_table_h = MA_table_h;
+k_table_pos = [k_table_posX k_table_posY k_table_col k_table_h];
+h.k_table_label = uicontrol (   "style", "text",
+                                "string", "Stiffness",
+                                "horizontalalignment", "center",
+                                "position", [k_table_pos(1) k_table_pos(2)+k_table_pos(4) k_table_pos(3) 20]
+);
+h.k_table = uitable (   "Data", f,
+                        "ColumnEditable",true,
+                        "ColumnName", {"k"},
+                        "RowName", "",
+                        "CellEditCallback", @updatePlot,
+                        "ColumnWidth", repmat({k_table_col}, 1, size(f,2)),
+                        "Position", k_table_pos
+);
+
 set (gcf, "color", get(0, "defaultuicontrolbackgroundcolor"))
 guidata (gcf, h)
 
